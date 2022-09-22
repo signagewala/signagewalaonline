e939e10cfd072c1d7b0e1ce9e4dcefee18dfd7a8
signagewala <signagewala.com@gmail.com>
2022-09-22 13:14:06 +0530

    fontawesomelinked

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import Script from 'next/script';
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
-export default MyApp
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+
+    const Layout = Component.layout || (({ children }) => <>{children}</>);
+
+    return (
+      <React.Fragment>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <title>Signagewala - App</title>
+          <meta name="description" content="Manage your retail branding and brand signages, all at one place" />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <Script src="https://kit.fontawesome.com/e0d2e0c039.js" crossorigin="anonymous"></Script>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </React.Fragment>
+    );
+  }
+}
